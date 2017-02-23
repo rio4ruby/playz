d0fb92240481485bc9b537c4ea685ea17bd038ff
Kit Kleckner <kit@kitatdotnet.net>
2017-02-23 12:16:32 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :pages
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "pages#index"
+  
+end
