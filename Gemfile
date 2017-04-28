23b372a6c9bd2bf75baccccc984deafddc3dacbc
Kit Kleckner <kit@kitatdotnet.net>
2017-04-28 00:12:59 -0700

    configuration files for deployment

--- Gemfile
+++ Gemfile
@@ -65,6 +65,9 @@ gem 'rio'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Use Mina for deployment
+gem 'mina'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
---
+++ install-libs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get install curl git patch build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion libpq-dev \
+     libmagic-dev \
+     libtag1-dev \
+     ruby-rmagick \
+     libmagickcore-6-arch-config \
+     libmagickcore-6-headers \
+     libmagickcore-6.q16-2 \
+     libmagickcore-6.q16-2-extra \
+     libmagickcore-6.q16-dev \
+     libmagickwand-6-headers \
+     libmagickwand-6.q16-2 \
+     libmagickwand-6.q16-dev \
+     libmagickwand-dev \
+     libmagickcore-dev \
+     libwebp-dev
+     # graphicsmagick-libmagick-dev-compat \
+
+# imagemagick - image manipulation programs -- binaries
+# imagemagick-6.q16 - image manipulation programs -- quantum depth Q16
+# imagemagick-common - image manipulation programs -- infrastructure
+# libgraphics-magick-perl - format-independent image processing - perl interface
+# libgraphicsmagick++-q16-12 - format-independent image processing - C++ shared library
+# libgraphicsmagick++1-dev - format-independent image processing - C++ development files
+# libgraphicsmagick-q16-3 - format-independent image processing - C shared library
+# libgraphicsmagick1-dev - format-independent image processing - C development files
+# libmagick++-6-headers - object-oriented C++ interface to ImageMagick - header files
+# libmagick++-6.q16-5v5 - object-oriented C++ interface to ImageMagick
+# libmagick++-6.q16-dev - object-oriented C++ interface to ImageMagick - development files
+# libmagick++-dev - object-oriented C++ interface to ImageMagick
+# libmagickcore-6-arch-config - low-level image manipulation library - architecture header files
+# libmagickcore-6-headers - low-level image manipulation library - header files
+# libmagickcore-6.q16-2 - low-level image manipulation library -- quantum depth Q16
+# libmagickwand-6-headers - image manipulation library - headers files
+# l
