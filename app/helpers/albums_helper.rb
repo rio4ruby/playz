a5d3a285374cb0960d9fce76ccbe2d03f6cee890
Kit Kleckner <kit@kitatdotnet.net>
2017-05-06 23:26:19 -0700

    work on albumart images

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 module AlbumsHelper
-  def albumart_url(album, _size = 'medium')
+  def coverart_url(album, size = 'medium')
+    artist = album.album_artist
+    if artist
+      album_name = album.name
+      artist_name = artist.name
+      path = "/#{artist_name}/#{album_name}/cover/#{size}.jpg"
+      url = "http://albumart.kitatdot.net" + path
+    end
+    url
+  end
+
+  def albumart_url(album, size = 'medium')
+    # coverart_url(album,size)
     image_file = album.album_image_file
     if image_file
       image_file.imageurl
