4b3eecdd350a7c0fc47d2bd2a755dc2da094e360
Christopher Kleckner <christopher.kleckner@rio4ruby.com>
2017-11-21 14:57:18 -0800

    play/pause button shows always

--- install-libs.sh
+++ install-libs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install curl git patch build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion libpq-dev \
+sudo apt-get install curl git patch build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion \
+     libpq-dev \
      libmagic-dev \
      libtag1-dev \
      ruby-rmagick \
